8c2278bd153eb9d89c20ad1014ea2387c25625e0
Ashton Cochrane <ashtoncochrane96@gmail.com>
2017-10-11 14:17:09 +1300

    Adding java doc

---
+++ BigInt.java
@@ -0,0 +1,37 @@
+
+public class BigInt {
+    public String stringVal;
+    public String remainder;
+    
+    
+    public BigInt(String input) {
+        this.stringValue = input;
+
+    }
+
+    public String toString() {
+        return stringValue;
+    }
+
+
+
+    public boolean isEqualTo(String s) {
+        if !(d.charAt(0).isEqual(s.charAt(0))) {
+                return false;
+                else {
+                    // either both +ve or -ve
+                    if (d.length() != s.length()) {
+                        return false;
+                    } else 
+                    
+
+        }
+
+        
+    }
+    
+    
+
+
+
+}
