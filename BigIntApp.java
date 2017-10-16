b4fc66f22d31374e519ef57de175f6cbe982e302
cocas692 <31051203+cocas692@users.noreply.github.com>
2017-10-16 13:41:28 +1300

    Completely working

--- BigIntApp.java
+++ BigIntApp.java
@@ -11,16 +11,25 @@ public class BigIntApp {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             readIn = sc.nextLine();
-            if (readIn != "" && readIn != "#") {
-                String[] input = readIn.split(" ");
-                if (input[0].matches("\\d+") && input.length == 3 && input[2].matches("\\d+")) {
-                    readOutput.add(readIn);
-                    output.add(input[0]);
-                    output.add(input[1]);
-                    output.add(input[2]);
+            if (!readIn.isEmpty() && readIn.charAt(0) != '#') {
+                if (readIn.charAt(0) != ' ') {
+                    String[] input = readIn.split(" ");
+                    if (input[0].matches("\\d+") && input.length == 3 && input[2].matches("\\d+")) {
+                        readOutput.add(readIn);
+                        output.add(input[0]);
+                        output.add(input[1]);
+                        output.add(input[2]);
+                    } else {
+                        readOutput.add(readIn);
+                        output.add("1");
+                        output.add("error");
+                        output.add("1");
+                    }
                 }
+                
             }
         }
+        
         int j = 0;
         for (int i = 0; i < output.size(); i+=3) {
             
@@ -29,43 +38,48 @@ public class BigIntApp {
             
             switch (output.get(i+1)) {
                 case "gcd":
-                    System.out.println(readOutput.get(j));
+                    System.out.println("\n" + readOutput.get(j));
                     System.out.println("# " + first.greatestCommonDivisor(second));
                     j++;
                     break;
                 case "-":
-                    System.out.println(readOutput.get(j));
-                    System.out.println("# " + first.subtract(second) + "\n");
+                    System.out.println("\n" + readOutput.get(j));
+                    System.out.println("# " + first.subtract(second));
                     j++;
                     break;
                 case "+":
-                    System.out.println(readOutput.get(j));
-                    System.out.println("# " + first.add(second) + "\n");
+                    System.out.println("\n" + readOutput.get(j));
+                    System.out.println("# " + first.add(second));
                     j++;
                     break;
                 case "/":
-                    System.out.println(readOutput.get(j));
-                    System.out.println("# " + first.divideBy(second) + "\n");
+                    System.out.println("\n" + readOutput.get(j));
+                    System.out.println("# " + first.divideBy(second));
                     j++;
                     break;
                 case "*":
-                    System.out.println(readOutput.get(j));
-                    System.out.println("# " + first.multiplyBy(second) + "\n");
+                    System.out.println("\n" + readOutput.get(j));
+                    System.out.println("# " + first.multiplyBy(second));
                     j++;
                     break;
                 case "<":
-                    System.out.println(readOutput.get(j));
-                    System.out.println("# " + first.isLessThan(second) + "\n");
+                    System.out.println("\n" + readOutput.get(j));
+                    System.out.println("# " + first.isLessThan(second));
                     j++;
                     break;
                 case ">":
-                    System.out.println(readOutput.get(j));
-                    System.out.println("# " + first.isGreaterThan(second) + "\n");
+                    System.out.println("\n" + readOutput.get(j));
+                    System.out.println("# " + first.isGreaterThan(second));
                     j++;
                     break;
                 case "=":
-                    System.out.println(readOutput.get(j));
-                    System.out.println("# " + first.isEqualTo(second) + "\n");
+                    System.out.println("\n" + readOutput.get(j));
+                    System.out.println("# " + first.isEqualTo(second));
+                    j++;
+                    break;
+                default:
+                    System.out.println("\n" + readOutput.get(j));
+                    System.out.println("# Syntax error");
                     j++;
                     break;
             }
